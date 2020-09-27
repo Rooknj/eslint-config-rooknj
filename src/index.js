4328f63f1c38ce1c56239d73a9f9ce2680d655cb
Nick Rook <rooknj@outlook.com>
2020-09-26 21:53:39 -0400

    chore: update index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+// See https://medium.com/@brygrill/create-react-app-with-typescript-eslint-prettier-and-github-actions-f3ce6a571c97 for base config setup
 module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
-  extends: [
-    "airbnb",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
@@ -23,22 +27,39 @@ module.exports = {
     },
     ecmaVersion: 2018,
     sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: ".",
   },
-  plugins: ["react", "@typescript-eslint", "react-hooks"],
   rules: {
-    "no-underscore-dangle": 0,
-    "no-unused-vars": "warn",
+    /* General */
 
-    "import/no-useless-path-segments": 1,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "import/newline-after-import": 1,
+    /* Typescript */
+    // Allows for implicit type assumption (ex: () => 2 assumes it returns a number. You don't need to specify)
+    "@typescript-eslint/explicit-module-boundary-types": ["off"],
 
-    "react/jsx-filename-extension": 0,
-    "react/prop-types": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "react/jsx-props-no-spreading": 0,
+    /* Imports */
+    "import/no-useless-path-segments": ["error"],
+    "import/newline-after-import": ["error"],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
 
-    "prettier/prettier": 0,
+    /* React */
+    // We don't need prop-types with typescript
+    "react/prop-types": ["off"],
+    "react/jsx-one-expression-per-line": ["off"],
+    // I like the {...props} syntax
+    "react/jsx-props-no-spreading": ["off"],
+
+    /* Prettier */
+    "prettier/prettier": ["error"],
   },
 };
